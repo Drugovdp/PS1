53fbfd5667f1836e5234e941478a9de08dbd2bb6
DMITRII DRUGOV <drugovdp@gmail.com>
2022-12-07 18:25:42 +0500

    Added title

---
+++ src/component/Tasks.tsx
@@ -0,0 +1,41 @@
+import './Tasks.css';
+
+type TasksPropsType = {
+    data: {
+        title: string,
+        tasks: Array<TasksType>,
+        students: Array<string>
+    }
+}
+
+type TasksType = {
+    taskId: number,
+    title: string,
+    isDone: boolean
+}
+
+export const Tasks = (props: TasksPropsType) => {
+    return (
+        <div className="tasks_el">
+            <h1>Главная страница</h1>
+            <h3>{props.data.title}</h3>
+            <ul>
+                {props.data.students.map((el, k) => {
+                    return (
+                        <li className="task_lists">
+                            <div className="task_lists-numlist">{k}:</div>
+                            <div className="task_lists-namelist">{el}</div>
+                            {props.data.tasks.map(e=> {
+                               return(
+                                   <div className="task_lists--valueChecbox">
+                                       <input type="checkbox" checked={e.isDone}/><span>{e.title}</span>
+                                   </div>
+                               )
+                            })}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
+}
